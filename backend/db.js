02d408e057b740f4be1ecc3ba4a3e4bdbb5e8c21
ankit gupta <ankitgupta17878@gmail.com>
2023-08-09 15:55:31 +0530

    1) first commit: dailyNote date: 9/8/23

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const mongoURI="mongodb://localhost:27017/?readPreference=primary&tls=false&directConnection=true";
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI)
+}
+module.exports = connectToMongo;  
